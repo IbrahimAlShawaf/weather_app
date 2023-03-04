5c1180e26d462d158105b43ab0b3a00b94757b06
IbrahimAlShawaf <a0594314120@rawafed.edu.ps>
2023-03-04 12:57:47 +0200

    weatherapp

---
+++ lib/controller/weather_api_controller.dart
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'dart:convert';
+
+
+// import 'package:get/get.dart';
+// import 'package:http/http.dart' as http;
+
+
+// class ApiService extends GetxController{
+//   Future<dynamic> getService() async {
+//     var url = Uri.parse(
+//         "https://api.openweathermap.org/data/2.5/weather?lat=31.9&lon=35.2&appid=37f788b5d4df634953b66ba4926dfcf1");
+  
+//       var response = await http.get(url);
+      
+//         if (response.statusCode == 200) {
+//       var body = jsonDecode(response.body);
+//       return body;
+//     }
+        
+      
+    
+//   }
+// }
---
+++ lib/view/my_home_page.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("data"),
+      ),
+      body: null,
+    );
+  }
+}
